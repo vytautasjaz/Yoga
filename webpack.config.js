add728b45d5cc25850dd6503af30dec78ed82226
vytautasjaz@gmail.com <vytautasjaz@gamil.com>
2020-01-20 19:15:20 +0200

    test

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+module.exports = {
+  entry: './src/index.js',
+  mode: 'development',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+        filename: 'app.css',
+    }),
+  ]
+};
