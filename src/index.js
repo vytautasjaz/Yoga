edf1d663e4a0149432eb188cd3e31605b20e5508
Vytautas <vytautasjaz@gamil.com>
2020-01-29 21:45:28 +0200

    Calendar finished

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import './assets/styles/app.scss';
 import './index.hbs';
-
 import './assets/images/logo-main-small.webp';
 import './assets/images/art-home-yora.jpg';
 import './assets/images/y086.jpg';
 import './assets/images/y080.jpg';
 import './assets/images/y006.jpg';
 import './assets/images/y093.jpg';
+import './assets/images/y006.jpg';
+import './assets/images/y016-300X300.jpg';
+import './assets/images/y052-900X900.jpg';
+
+
+
+
 
 
