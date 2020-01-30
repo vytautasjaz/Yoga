c5eadf64b3d0922af24e67734ab674dc671936b2
Vytautas <vytautasjaz@gamil.com>
2020-01-30 18:25:39 +0200

    Topnav

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import './assets/styles/app.scss';
 import './index.hbs';
 import './assets/images/logo-main-small.webp';
 import './assets/images/art-home-yora.jpg';
+import './assets/images/y048.jpg';
 import './assets/images/y086.jpg';
 import './assets/images/y080.jpg';
 import './assets/images/y006.jpg';
